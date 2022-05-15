00605a8f33177ea12686e3ab2f461e02bf5ececb
zehra-bak <104216027+zehra-bak@users.noreply.github.com>
2022-05-15 19:30:09 +0300

    Add files via upload

---
+++ mesaj.php
@@ -0,0 +1,7 @@
+<?php
+echo "Kullanıcı Adı:".$_POST['ad'];
+echo "Kullanıcı Email:".$_POST['email'];
+echo "Kullanıcı Telefon:".$_POST['telefon'];
+echo "Kullanıcı Website:".$_POST['website'];
+echo "Mesaj:".$_POST['mesaj'];
+?>
